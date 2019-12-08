02dd3e5cb8c19f9a86a4548e71cd74c0464ce8cb
dovip <dovip@sela.co.il>
2019-12-08 19:31:08 +0200

    mutation

--- server/app.js
+++ server/app.js
@@ -2,8 +2,15 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+
+mongoose.connect("mongodb+srv://dovip:abcd1234@cluster0-2slea.azure.mongodb.net/test?retryWrites=true&w=majority");
+mongoose.connection.once('open',()=>{
+    console.log('the db is open');
+})
 
 app.use('/graphql', graphqlHTTP({
     schema,
