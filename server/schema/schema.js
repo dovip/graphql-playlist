b29d6790299b6a0029ba2566a4eecb2854bfe32a
dovip <dovip@sela.co.il>
2019-12-08 19:53:53 +0200

    add list for query

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -69,6 +69,18 @@ const RootQuery = new GraphQLObjectType({
                 //code to get from db
                 return _.find(authors, { id: args.id });
             }
+        },
+        books:{
+            type:new GraphQLList(BookType),
+            resolve(parent,args){
+                return books;
+            }
+        },
+        authors:{
+            type:new GraphQLList(AuthorType),
+            resolve(parent,args){
+                return authors;
+            }
         }
     }
 })
